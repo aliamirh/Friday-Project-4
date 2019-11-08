523994f7a35a4b0d5a33012f70a57e8198d8ac7b
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-08 13:31:29 -0800

    add pizza constructors and methods. still messing around to see how to do this the best way

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+Pizza = function(personName, type, size){
+  this.personName = personName,
+  this.type = type,
+  this.size = size,
+  this.price = 0
+}
+PizzaOrder = function(){
+  this.pizzas = []
+}
+
+PizzaOrder.prototype.addPizzas = function(pizzaOrder){
+  this.pizzas.push(pizzaOrder)
+}
