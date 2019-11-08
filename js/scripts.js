6212706c93bca888221226d3ce381de1ef22dc74
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-08 15:30:05 -0800

    trouble shooting issue with not getting price

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,59 @@
 Pizza = function(personName, topping, size){
-  this.personName = personName,
-  this.topping = topping,
-  this.size = size,
-  this.price = 0
-}
-PizzaOrder = function(){
-  this.pizzas = []
+  this.personName = personName;
+  this.topping = topping;
+  this.size = size;
+  this.price = 0;
 }
 
-PizzaOrder.prototype.addPizzas = function(pizzaOrder){
-  this.pizzas.push(pizzaOrder)
+Pizza.prototype.getTotal = function(){
+  if(this.size === "Large"){
+    console.log("adfsd")
+    this.price += 10;
+  }else if(this.size === "Medium"){
+    this.price += 8;
+  }else if( this.size === "Large"){
+    this.price += 5;
+  }else{
+    console.log("test1")
+  }
+  return this.price;
 }
 
-function sizePrice(){
-  if(PizzaOrder.pizzas[0].size === "Small"){
-    PizzaOrder.pizzas[0].price = 5;
+// PizzaOrder = function(){
+//   this.pizzas = []
+// }
+// PizzaOrder.prototype.addPizzas = function(pizzaOrder){
+//   this.pizzas.push(pizzaOrder)
+// }
+//
+//
+//
+// function sizePrice(){
+//   if(PizzaOrder.pizzas[0].size === "Small"){
+//     PizzaOrder.pizzas[0].price = 5;
+//   }else if (PizzaOrder.pizzas[0].size === "Medium"){
+//     PizzaOrder.pizzas[0].price = 8;
+//   }else{
+//     PizzaOrder.pizzas[0].price = 12;
+//   }
+//
+// }
+//
+// function toppingPrice(){
+//     if(PizzaOrder.pizzas[0].topping === "Pepperoni"){
+//       PizzaOrder.pizzas[0].price += 1;
+//     }else if (PizzaOrder.pizzas[0].topping === "Peppers"){
+//       PizzaOrder.pizzas[0].price += 1;
+//     }else if (PizzaOrder.pizzas[0].topping === "Cheese"){
+//       PizzaOrder.pizzas[0].price += 2;
+//     }else if (Pizza.pizzas[0].topping === "Vegan"){
+//       PizzaOrder.pizzas[0].price += 3;
+//     }
+//
+//
+// }
+
+
 
 
 
@@ -41,8 +80,12 @@ $(document).ready(function(){
     var personNameInput = $("#personName").val();
     var pizzaSizeInput = $("input[name='pizza']:checked").val();
     var pizzaToppingInput = $("input[name='topping']:checked").val();
-    console.log(pizzaToppingInput)
-    console.log(pizzaSizeInput)
+    var myPizza = new Pizza (personNameInput, pizzaSizeInput, pizzaToppingInput)
+    var totalPrice = myPizza.getTotal();
+    console.log(totalPrice);
     console.log(personNameInput)
+    console.log(pizzaSizeInput)
+    console.log(pizzaToppingInput)
+
   });
 });
