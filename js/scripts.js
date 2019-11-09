980ee7e92db62a2da053d271ca6535c928e5e2d4
Ali Amirhamzeh <aliamirhamzeh@gmail.com>
2019-11-08 16:06:15 -0800

    total up pizza and display it on the html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,75 +1,25 @@
-Pizza = function(personName, topping, size){
+Pizza = function(personName, size, topping){
   this.personName = personName;
-  this.topping = topping;
   this.size = size;
+  this.topping = topping;
   this.price = 0;
 }
 
 Pizza.prototype.getTotal = function(){
-  if(this.size === "Large"){
-    console.log("adfsd")
-    this.price += 10;
-  }else if(this.size === "Medium"){
-    this.price += 8;
-  }else if( this.size === "Large"){
-    this.price += 5;
+  if(this.size === "small"){
+    return this.price += 5;
+
+  }else if(this.size === "medium"){
+    return this.price += 8;
+
+  }else if( this.size === "large"){
+    return this.price += 10;
+
   }else{
-    console.log("test1")
+
   }
   return this.price;
 }
-
-// PizzaOrder = function(){
-//   this.pizzas = []
-// }
-// PizzaOrder.prototype.addPizzas = function(pizzaOrder){
-//   this.pizzas.push(pizzaOrder)
-// }
-//
-//
-//
-// function sizePrice(){
-//   if(PizzaOrder.pizzas[0].size === "Small"){
-//     PizzaOrder.pizzas[0].price = 5;
-//   }else if (PizzaOrder.pizzas[0].size === "Medium"){
-//     PizzaOrder.pizzas[0].price = 8;
-//   }else{
-//     PizzaOrder.pizzas[0].price = 12;
-//   }
-//
-// }
-//
-// function toppingPrice(){
-//     if(PizzaOrder.pizzas[0].topping === "Pepperoni"){
-//       PizzaOrder.pizzas[0].price += 1;
-//     }else if (PizzaOrder.pizzas[0].topping === "Peppers"){
-//       PizzaOrder.pizzas[0].price += 1;
-//     }else if (PizzaOrder.pizzas[0].topping === "Cheese"){
-//       PizzaOrder.pizzas[0].price += 2;
-//     }else if (Pizza.pizzas[0].topping === "Vegan"){
-//       PizzaOrder.pizzas[0].price += 3;
-//     }
-//
-//
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //----------------UI--------------------
 
@@ -78,14 +28,15 @@ $(document).ready(function(){
   $(".formGroup").submit(function(event){
     event.preventDefault()
     var personNameInput = $("#personName").val();
-    var pizzaSizeInput = $("input[name='pizza']:checked").val();
     var pizzaToppingInput = $("input[name='topping']:checked").val();
+    var pizzaSizeInput = $("input[name='pizza']:checked").val();
     var myPizza = new Pizza (personNameInput, pizzaSizeInput, pizzaToppingInput)
     var totalPrice = myPizza.getTotal();
-    console.log(totalPrice);
-    console.log(personNameInput)
-    console.log(pizzaSizeInput)
-    console.log(pizzaToppingInput)
+    $("#person-name").text(personNameInput);
+    $("#pizza-size").text(pizzaSizeInput);
+    $("#topping").text(pizzaToppingInput);
+    $("#total").text(totalPrice);
+    $("#reciept").show();
 
   });
 });
